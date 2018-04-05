500a5a988f86a8f345c52b7eee06c4388fc124f2
Alec Koumjian <akoumjian@gmail.com>
2018-04-05 10:42:52 -0400

    Welcome message

---
+++ assets/js/welcome.js
@@ -0,0 +1,8 @@
+function welcome() {
+    console.log('Hello!')
+    console.log('This site was lovingly crafted by hand in the hills of Brattleboro, Vermont.')
+    console.log('It features opengraph and schema.org metadata so you can ingest it intelligently.')
+    console.log('The source is right over here → https://github.com/instig8/hackathon')
+}
+
+welcome()
