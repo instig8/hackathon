f849d23d1cf7fe2eeafa8879d6541c1105bd9e30
Alec Koumjian <akoumjian@gmail.com>
2018-05-18 21:31:23 -0400

    include menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
 function initialize() {
     const menuButton = document.querySelector('.menu')
+    const navLinks = document.querySelectorAll('nav a')
     const body = document.querySelector('body')
     menuButton.addEventListener('click', (evt) => {
         evt.stopPropagation()
         body.classList.toggle('menu-open')
+    })
+    navLinks.forEach((link) => {
+        link.addEventListener('click', (evt) => {
+            evt.stopPropagation()
+            body.classList.toggle('menu-open')
+        })
     })
 }
 
--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -27,34 +27,10 @@ function renderStyles() {
     return Array.from(arguments).map((style) => style.join(';')).join(';')
 }
 
-// Create a global listener for the email property
-var _email = '';
-
-Object.defineProperty(window, 'email', {
-    get: function () {
-        return _email;
-    },
-    set: function (input) {
-        _email = input;
-        submitEmail(_email)
-    }
-});
-
-function submitEmail(email) {
-    var emailInput = document.querySelector('input[name="email"]')
-    var form = document.querySelector('#emailForm')
-
-    emailInput.value = email
-    form.submit()
-}
-
 function welcome() {
     console.log('%cHello!', renderStyles(baseStyles, headerStyles))
     console.log('%cThis site was lovingly hand crafted in the hills of Brattleboro, Vermont. \n' + 
-                'It features opengraph and schema.org metadata so you can ingest it intelligently.\n' + 
-                'The source is right over here → https://github.com/instig8/hackathon \n' + 
-                'Enter your email as shown below and we\'ll keep you in the loop about the hackathon (you may get redirected).', renderStyles(baseStyles, pStyles))
-    console.log('%cemail = "your@email.com"', renderStyles(codeStyles))
+                'It features opengraph and schema.org metadata so you can ingest it intelligently.\n', renderStyles(baseStyles, pStyles))
 }
 
 welcome()
